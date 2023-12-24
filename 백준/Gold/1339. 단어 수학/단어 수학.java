3c6a4e53d950b71022eee7ca1448758242b5a5ee
juan-jung <118125595+juan-jung@users.noreply.github.com>
2023-12-24 22:26:56 +0900

    [Gold IV] Title: 단어 수학, Time: 80 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,5 +1,3 @@
-
-
 import java.io.*;
 import java.util.*;
 
@@ -7,35 +5,19 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		List<int[]> count = new ArrayList<>();
-		for(int i=0;i<26;i++) count.add(new int[] {i,0});
-		String[] words = new String[N];
+		int[] count = new int[26];
 		for(int i=0;i<N;i++) {
-			words[i] = br.readLine();
-			for(int j=0;j<words[i].length();j++) {
-				char c = words[i].charAt(j);
-				count.get(c-'A')[1] += Math.pow(10,words[i].length() - j - 1); 
-			}
+			String word = br.readLine();
+			for(int j=0;j<word.length();j++) count[word.charAt(j)-'A'] += Math.pow(10, word.length()-j-1); 
 		}
 		
-		Collections.sort(count, (o1,o2)->Integer.compare(o2[1],o1[1]));
-		
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		int v = 9;
-		for(int[] a : count) {
-			if(a[1] == 0) break;
-			map.put(a[0], v--);
-		}
+		Arrays.sort(count);
 		
 		int ans = 0;
-		for(int i=0;i<N;i++) {
-			String sum = "";
-			for(int j=0;j<words[i].length();j++) {
-				sum += map.get(words[i].charAt(j)-'A');
-			}
-			ans += Integer.parseInt(sum);
+		for(int i=25;i>0;i--) {
+			if(count[i]==0) break;
+			ans += count[i] * (i-16);
 		}
-		
 		
 		System.out.println(ans);
 		br.close();
