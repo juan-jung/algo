08d7636447123c9628d1ac68916463a5e82d252b
juan-jung <118125595+juan-jung@users.noreply.github.com>
2024-04-07 00:12:52 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 80 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -1,3 +1,5 @@
+
+
 import java.io.*;
 import  java.util.*;
 
@@ -8,7 +10,8 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         char[][] box = new char[N][M];
-        int[] init = new int[5]; // rx, ry, bx, by, cnt
+        int[] init = new int[6]; // rx, ry, bx, by, cnt, d
+        init[5] = -1;
         for(int i=0;i<N;i++) {
             String row = br.readLine();
             for(int j=0;j<M;j++) {
@@ -27,30 +30,51 @@ public class Main {
         }
 
         //도달 할 수 있는 최대 지점 미리 계산
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
         int[][][] dist = new int[N][M][4];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (box[i][j] == '#' || box[i][j] == 'O') continue; // 벽이거나 구멍이면 건너뛰기
-                for (int d = 0; d < 4; d++) {
-                    int nx = i, ny = j;
-                    while (true) {
-                        nx += dx[d];
-                        ny += dy[d];
-                        if (nx < 0 || nx >= N || ny < 0 || ny >= M || box[nx][ny] == '#') break; // 벽을 만나거나 범위를 벗어나면 중지
-                        dist[i][j][d]++;
-                        if (box[nx][ny] == 'O') break; // 구멍에 도착하면 중지
-                    }
+
+        //상좌
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<M;j++) {
+                if(box[i][j]=='#') {
+                    dist[i][j][0] = -1;
+                    dist[i][j][2] = -1;
+                }
+                else if(box[i][j]=='O') {
+                    dist[i][j][0] = 0;
+                    dist[i][j][2] = 0;
+                }
+                else {
+                    dist[i][j][0] = dist[i-1][j][0] + 1;
+                    dist[i][j][2] = dist[i][j-1][2]+1;
+                }
+            }
+        }
+
+        //하우
+        for(int i=N-1;i>=0;i--) {
+            for(int j=M-1;j>=0;j--) {
+                if(box[i][j]=='#') {
+                    dist[i][j][1] = -1;
+                    dist[i][j][3] = -1;
+                }
+                else if(box[i][j]=='O') {
+                    dist[i][j][1] = 0;
+                    dist[i][j][3] = 0;
+                }
+                else {
+                    dist[i][j][1] = dist[i+1][j][1]+1;
+                    dist[i][j][3] = dist[i][j+1][3]+1;
                 }
             }
         }
 
         int ans = -1;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> Integer.compare(o1[4],o2[4])));
+        ArrayDeque<int[]> pq = new ArrayDeque<>();
         pq.offer(init);
         boolean[][][][] v = new boolean[N][M][N][M];
         v[init[0]][init[1]][init[2]][init[3]] = true;
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
 
@@ -62,7 +86,8 @@ public class Main {
             }
 
             for(int d=0;d<4;d++) {
-                int[] moved = new int[5];
+                if((d==0&&cur[5]==1) || (d==1&&cur[5]==0) || (d==2&&cur[5]==3) || (d==3&&cur[5]==2)) continue;
+                int[] moved = new int[6];
                 moved[0] = cur[0] + dx[d]*dist[cur[0]][cur[1]][d];
                 moved[1] = cur[1] + dy[d]*dist[cur[0]][cur[1]][d];
                 moved[2] = cur[2] + dx[d]*dist[cur[2]][cur[3]][d];
@@ -84,6 +109,7 @@ public class Main {
                 if(v[moved[0]][moved[1]][moved[2]][moved[3]]) continue;
                 v[moved[0]][moved[1]][moved[2]][moved[3]] = true;
                 moved[4]=cur[4]+1;
+                moved[5]= d;
                 pq.offer(moved);
             }
         }
