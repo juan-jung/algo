60c8a1d0267bc4c1052c48e01d729272f1c98c47
juan-jung <118125595+juan-jung@users.noreply.github.com>
2024-06-17 17:31:27 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -7,19 +7,15 @@ class Solution {
         int answer = 0;
         
         Set<Integer>[] dp = new HashSet[9];
-        for(int i=0;i<9;i++) {
-            dp[i] = new HashSet<Integer>();
-        }
-        
-        int temp = N;
-        dp[1].add(temp);
-        temp*=10;
+        for(int i=0;i<9;i++) dp[i] = new HashSet<Integer>();        
+        int NN = N;
+        dp[1].add(NN);
+        NN = NN*10+N;
         
         for(int i=2;i<9;i++) {
             //연산자없는거 넣어주고
-            temp+=N;
-            dp[i].add(temp);
-            temp*=10;
+            dp[i].add(NN);
+            NN=NN*10+N;
             
             //사칙연산
             for(int j=1;j<i;j++) {
@@ -27,26 +23,18 @@ class Solution {
                     for(int b : dp[j]) {
                         //덧셈
                         dp[i].add(a+b);
-                        
                         //뺄셈
                         dp[i].add(a-b);
-                        // dp[i].add(b-a);
                         //나눗셈
                         if(b!=0) dp[i].add(a/b);
-                        // if(a!=0) dp[i].add(b/a);
                         //곱셈
                         dp[i].add(a*b);
-                        // dp[i].add(b*a);
                     }
                 }
             }
             
             if(dp[i].contains(number)) return i;
         }
-        
-        // for(int i=1;i<9;i++) {
-        //     System.out.println(dp[i]);
-        // }
         return -1;
     }
 }
