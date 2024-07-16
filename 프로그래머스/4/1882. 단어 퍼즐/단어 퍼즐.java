4f6c5b1a7b87635c042f0bcb708c70a7bd76ff10
juan-jung <118125595+juan-jung@users.noreply.github.com>
2024-07-16 17:31:48 +0900

    [level 4] Title: 단어 퍼즐, Time: 20.52 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/4/1882. 단어 퍼즐/단어 퍼즐.java
+++ 프로그래머스/4/1882. 단어 퍼즐/단어 퍼즐.java
@@ -5,24 +5,20 @@ class Solution {
         Set<String> strs_set = new HashSet<>();
         Collections.addAll(strs_set, strs);
         
-        // Initialize the dp array where dp[i] represents the minimum number of substrings needed to form t[0:i]
-        int n = t.length();
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0; // No substrings needed to form an empty string
-
-        // Dynamic programming to fill the dp array
-        for (int i = 1; i <= n; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (strs_set.contains(t.substring(j, i))) {
-                    if (dp[j] != Integer.MAX_VALUE) {
-                        dp[i] = Math.min(dp[i], dp[j] + 1);
-                    }
+        int tLen = t.length();
+        int[] dp =  new int[tLen+1];
+        final int INF =  Integer.MAX_VALUE;
+        Arrays.fill(dp,INF);
+        dp[tLen] = 0;
+        
+        for(int i=tLen-1;i>=0;i--) {
+            for(int j=0;j<5 && i+j<tLen;j++) {
+                if(dp[i+j+1]!=INF  &&  strs_set.contains(t.substring(i,i+j+1))) {
+                    dp[i] =  Math.min(dp[i], dp[i+j+1]+1);
                 }
             }
         }
-
-        // The answer is in dp[n] if it's not Integer.MAX_VALUE, else it's not possible to form t
-        return dp[n] == Integer.MAX_VALUE ? -1 : dp[n];
+       
+        return dp[0]==INF ? -1 : dp[0];
     }
 }
