762b3dc20b1d62b97dd2b82bf25ec3b3d3823ca8
juan-jung <118125595+juan-jung@users.noreply.github.com>
2024-08-08 22:57:20 +0900

    [Bronze II] Title: 영식이와 친구들, Time: 92 ms, Memory: 12828 KB -BaekjoonHub

--- 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
+++ 백준/Bronze/1592. 영식이와 친구들/영식이와 친구들.java
@@ -1,5 +1,1 @@
-import java.util.*;
-public class Main {public static void main(String[] args) {Scanner s = new Scanner(System.in);
-      int a = s.nextInt(), b=s.nextInt(),L =s.nextInt(),g=0;
-      for(int i=1;i<=L; i++) {if(a%i==0 && L%i==0) g=i;}
-         System.out.println((a/g)*(b-1));}}
+import java.util.*;public class Main {public static void main(String[] args) {Scanner s = new Scanner(System.in);int a=s.nextInt(),b=s.nextInt(),L=s.nextInt(),g=0;for(int i=1;i<=L;i++){if(a%i==0&&L%i==0)g=i;}System.out.println((a/g)*(b-1));}}
