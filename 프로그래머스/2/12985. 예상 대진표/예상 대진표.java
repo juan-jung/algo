ec2d587fc2b40c5cdad697962e8ff25bf723fd27
juan-jung <118125595+juan-jung@users.noreply.github.com>
2024-11-07 21:39:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,18 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        
+        a -= 1;
+        b -= 1;
+        
+        while(a!=b) {
+            a /= 2;
+            b /= 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
